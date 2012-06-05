4402897d0827ac8569ce9a29c497d4f4265d56fb
Alin Predoi <predoialin@gmail.com>
2012-06-06 01:34:56 +0300

    * added custom tags for username / full name

--- main/templatetags/user_tags.py
+++ main/templatetags/user_tags.py
@@ -1,1 +1,19 @@
-__author__ = 'apredoi'
+from django import template
+from django.contrib.auth.models import User
+
+register = template.Library()
+
+@register.simple_tag
+def get_full_name(user):
+    name = user.get_full_name()
+    if not name :
+        name = user.username
+    return name
+
+
+@register.simple_tag
+def get_first_name(user):
+    name = user.first_name
+    if not name :
+        name = user.username
+    return name
