b1de42adb52eedd10169f1863e0a5767e25cdaef
Alin Predoi <predoialin@gmail.com>
2012-06-17 22:56:09 +0300

    remove duplicate entry

--- main/backend/views.py
+++ main/backend/views.py
@@ -3,6 +3,7 @@ from django.template.context import RequestContext
 from django.shortcuts import render, render_to_response
 from django.contrib.auth.models import User
 from django.utils import simplejson
+from django.utils.datastructures import MultiValueDictKeyError
 from main.decorators import login_required
 from main.backend.forms import ProfileForm, SiteConfigForm
 from main.models import SiteConfig
@@ -73,31 +74,30 @@ def update_profile(request):
 @login_required
 def add_site_config(request):
     if request.method == 'POST':
+        object_key=None
+        try:
+            object_key = request.POST['site_config_id']
+        except MultiValueDictKeyError:
+            pass
         instance = None
-        if request.POST['site_config_action'] == 'update':
-            instance = SiteConfig.objects.get(user=request.user, api_key=request.POST['api_key'], api_secret=request.POST['api_secret'])
+        action = request.POST['site_config_action']
+        if action in ('update', 'delete'):
+            instance = SiteConfig.objects.get(user=request.user, pk=object_key)
+
+        if instance and action == 'delete':
+            instance.delete()
+            return HttpResponse('{"action":"delete", "status":"ok", "id":"'+ object_key +'"}', mimetype='application/json')
+
         form = SiteConfigForm(request.POST,instance=instance) if instance else SiteConfigForm(request.POST)
         if form.is_valid():
             site_config = form.save(commit=False)
             site_config.user = request.user
             site_config.save()
         else:
-            return HttpResponse(simplejson.dumps({'error': 'inval form'}), mimetype='application/json' )
+            return HttpResponse(simplejson.dumps({'error': 'inval form'}), mimetype='application/json'
+            )
+        return HttpResponse('{"action":"' + action + '","status": "ok", "id":"'+ str(form.instance.pk) +'", '
+                                '"item":"'+ form.as_table().replace('"','\\"').replace('\n','').replace('\r','') +'"}',
+            mimetype='application/json')
 
-    return HttpResponse(simplejson.dumps({'status': 'ok'}), mimetype='application/json' )
-
-@login_required
-def add_site_config(request):
-    if request.method == 'POST':
-        instance = None
-        if request.POST['site_config_action'] == 'update':
-            instance = SiteConfig.objects.get(user=request.user, api_key=request.POST['api_key'], api_secret=request.POST['api_secret'])
-        form = SiteConfigForm(request.POST,instance=instance) if instance else SiteConfigForm(request.POST)
-        if form.is_valid():
-            site_config = form.save(commit=False)
-            site_config.user = request.user
-            site_config.save()
-        else:
-            return HttpResponse(simplejson.dumps({'error': 'inval form'}), mimetype='application/json' )
-
-    return HttpResponse(simplejson.dumps({'status': 'ok'}), mimetype='application/json' )
+    return HttpResponse('{"status": "ok"}', mimetype='application/json')
