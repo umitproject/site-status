fc1467847443d8956aae34989a3e83a1f9fc8352
Alin Predoi <predoialin@gmail.com>
2012-07-31 23:33:30 +0300

    fix url imports

--- status_cron/urls.py
+++ status_cron/urls.py
@@ -22,12 +22,11 @@
 from django.conf.urls.defaults import *
 import django_cron
 
-django_cron.autodiscover()
 
 urlpatterns = patterns('',
-    url('^check_passive_hosts/?$', 'status_cron.views.check_passive_hosts', name='check_passive_hosts'),
+#    url('^check_passive_hosts/?$', 'status_cron.views.check_passive_hosts', name='check_passive_hosts'),
     url('^check_passive_hosts_task/(?P<module_key>[0-9a-zA-Z\-\_]+)/?$', 'status_cron.views.check_passive_hosts_task', name='check_passive_hosts_task'),
     url('^aggregate_daily_status/?$', 'status_cron.views.aggregate_daily_status', name='aggregate_daily_status'),
-    url('^check_notifications/?$', 'status_cron.views.check_notifications', name='check_notifications'),
+#    url('^check_notifications/?$', 'status_cron.views.check_notifications', name='check_notifications'),
     url('^send_notification_task/(?P<notification_id>[0-9a-zA-Z\-\_]+)/?$', 'status_cron.views.send_notification_task', name='send_notification_task'),
 )
