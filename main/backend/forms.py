f588038d0a382cc6f65d53c89b121c501b2529dd
Alin Predoi <predoialin@gmail.com>
2012-08-21 21:04:07 +0300

    * fixed just added form inputs

--- main/backend/forms.py
+++ main/backend/forms.py
@@ -84,12 +84,10 @@ class ModuleForm(forms.ModelForm):
                                         help_text="HTTP response status code.")
     search_keyword = forms.CharField(required=False, widget=forms.TextInput(attrs=dict({'class':'search_keyword'})),
                                         help_text="Keyword or regex to search for in the result page.")
-    fail_on_error = forms.BooleanField(default=True,
-                                        widget=forms.CheckboxInput(attrs=dict({'class':'fail_on_error'})),
-                                        help_text="Returns with failure if the status code is greater than 300.")
-    follow_location = forms.BooleanField(default=True,
-                                        widget=forms.CheckboxInput(attrs=dict({'class':'follow_location'})),
-                                        help_text="Follow redirects.")
+    fail_on_error = forms.BooleanField(widget=forms.CheckboxInput(attrs=dict({'class':'fail_on_error'})),
+                                        help_text="Returns with failure if the status code is greater than 300.", required=False)
+    follow_location = forms.BooleanField(widget=forms.CheckboxInput(attrs=dict({'class':'follow_location'})),
+                                        help_text="Follow redirects.", required=False)
     status = forms.CharField(widget=forms.TextInput(attrs=dict({'class':'disabled', 'readonly':'readonly', 'disabled':'disabled' })), required=False)
 
     class Meta:
